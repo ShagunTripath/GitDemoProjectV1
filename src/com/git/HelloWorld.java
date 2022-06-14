a90bb3f34110a5ba1ddab0059a1c64370f899dbc
Dell <shaguntripathi549@gmail.com>
2022-06-14 09:38:12 +0530

    first commit......

---
+++ src/com/git/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hi";
+		
+		if(s!=null)
+			System.out.println("eat somethng......");
+		else
+			System.out.println("sleep");
+
+	}
+
+}
